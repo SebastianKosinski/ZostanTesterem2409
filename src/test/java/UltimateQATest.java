85e12267bf3d4812fc51120dd02331c7c9598b8e
Sebastian <sebastian.kosinski87@gmail.com>
2023-09-24 17:20:45 +0200

    Próba przesłania

---
+++ src/test/java/UltimateQATest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UltimateQATest extends PageSetup {
+
+
+    @Test
+    public void testOne() throws InterruptedException {
+        WebElement button = driver.findElementById( "idExample");
+        button.click();
+Thread.sleep(2000);
+        WebElement text = driver.findElementByClassName("entry-title");
+        Assertions.assertEquals( "Button success", text.getText());
+    }
+@Test
+    public void testTwo() throws InterruptedException { WebElement nameinput = driver.findElementById("et_pb_contact_name_0");
+
+    nameinput.sendKeys("Tester");
+    Thread.sleep(2000);
+    WebElement emailInput = driver.findElementById("et_pb_contact_email_0");
+
+    emailInput.sendKeys("tester@tester.pl");
+
+    Thread.sleep(2000);
+
+    WebElement buttonEmailMe  = driver.findElementByName("et_builder_submit_button");
+    buttonEmailMe.click();
+
+    }
+
+}
